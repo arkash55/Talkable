e81c0233e003fa00cd9ab4303e44df9be6391846
arkash55 <arkash.vijay55@gmail.com>
2025-08-01 20:14:15 +0100

    configured firebase sdk

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKpAsgT_-O7gdsFXPf7XFtMWbZh_QH4eI",
+  authDomain: "talkable-cf88e.firebaseapp.com",
+  projectId: "talkable-cf88e",
+  storageBucket: "talkable-cf88e.firebasestorage.app",
+  messagingSenderId: "544399554291",
+  appId: "1:544399554291:web:242dd206a8e928d898e986"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
