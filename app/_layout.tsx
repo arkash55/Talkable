6d8df7479eb2db31fe9d8d658b1723248b43af9d
arkash55 <arkash.vijay55@gmail.com>
2025-08-01 19:52:48 +0100

    configured light/dark mode

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
-import { Stack } from "expo-router";
+// app/_layout.tsx
+import { ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { useColorScheme } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { DarkTheme, LightTheme } from './themes/theme';
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme(); 
+
+  return (
+    <SafeAreaView>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : LightTheme}>
+        <Stack screenOptions={{ headerShown: false }}/>
+      </ThemeProvider>
+    </SafeAreaView>
+
+  );
 }
